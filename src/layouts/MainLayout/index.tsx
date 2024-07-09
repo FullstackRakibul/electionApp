c66b5729e662a558ed87ff8d96b8387a67de0238
FullstackRakibul <rakibul.cse.47@gmail.com>
2024-07-09 23:24:39 +0600

    Menu chages , /header and footer added/

--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -1,7 +1,8 @@
 import React, { ReactNode } from 'react';
-import { Layout, Menu } from 'antd';
-import { Outlet, useNavigate } from 'react-router-dom';
-const { Header, Sider, Content } = Layout;
+import { Layout } from 'antd';
+import MianFooter from '../footer';
+import MainHeader from '../header';
+const {  Content } = Layout;
 
 type MainLayoutProps = {
   children: ReactNode;
@@ -9,46 +10,24 @@ type MainLayoutProps = {
 
 
 const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
-  const navigate = useNavigate();
-  const handleMenuClick = (e: { key: string }) => {
-    navigate(e.key);
-  };
+
   return (
     <>
     <Layout style={{ minHeight: '100vh' }}>
-      <Layout>
-        <Sider width={200} className="site-layout-background">
-          <Menu
-            mode="inline"
-            defaultSelectedKeys={['1']}
-            style={{ height: '100%', borderRight: 0 }}
-          >
-            <Menu.Item key="/core/dashboard" onClick={handleMenuClick}>
-              Core Dashboard
-            </Menu.Item>
-            <Menu.Item key="/audit/dashboard" onClick={handleMenuClick}>
-              Audit Dashboard
-            </Menu.Item>
-            <Menu.Item key="/audit/about" onClick={handleMenuClick}>
-              About
-            </Menu.Item>
-            <Menu.Item key="/audit/terms" onClick={handleMenuClick}>
-              Terms
-            </Menu.Item>
-          </Menu>
-        </Sider>
-        <Layout style={{ padding: '0 24px 24px' }}>
+        <Layout style={{ padding: '24px 24px' }}>
+          <MainHeader/>
           <Content
             style={{
               padding: 24,
               margin: 0,
               minHeight: 280,
+              
             }}
           >
             {children}
           </Content>
         </Layout>
-      </Layout>
+        <MianFooter/>
     </Layout>
     </>
   )
